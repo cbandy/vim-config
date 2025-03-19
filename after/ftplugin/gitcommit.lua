a6da3d06a39ef79313ada7773aa72d486e536f1a
Chris Bandy <bandy.chris@gmail.com>
2025-03-18 19:32:11 -0500

    Explain the 50-character width of Git commit messages

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,5 +1,15 @@
 local vim = vim
 
--- highlight column 50 and 'textwidth' [:help colorcolumn]
-vim.opt_local.colorcolumn = '50,+0'
-vim.opt_local.spell = true
+-- The builtin "gitcommit" syntax highlights characters that extend beyond
+-- this length on the first line. [$VIMRUNTIME/syntax/gitcommit.vim]
+--
+-- The DISCUSSION section of git-commit(1) recommends that the first line
+-- of a commit message be a "short (less than 50 character)" summary.
+--
+-- The builtin "gitcommit" plugin sets 'textwidth' so highlight both of
+-- these columns by default. [$VIMRUNTIME/ftplugin/gitcommit.vim]
+vim.g.gitcommit_summary_length = 50
+vim.opt_local['colorcolumn'] = vim.g.gitcommit_summary_length .. ',+0'
+
+-- highlight misspelled words
+vim.opt_local['spell'] = true
