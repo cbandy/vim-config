eae6c094ff3726820b808e02fb9ec94c88c7905b
Chris Bandy <bandy.chris@gmail.com>
2025-08-10 22:05:02 -0500

    Add an offset to injected content inside YAML
    The block style indicator would cause an error in some injected parsers.

--- before/queries/yaml/injections.scm
+++ before/queries/yaml/injections.scm
@@ -2,10 +2,13 @@
 ; https://neovim.io/doc/user/treesitter.html#_treesitter-language-injections
 
 ; interpret multiline values (block scalars) of key-value pairs as the language of the key
-; https://yaml-multiline.info/#block-scalars
-(block_mapping_pair
-  key: (flow_node
-         (plain_scalar
-           (string_scalar) @injection.language (#any-of? @injection.language "json" "sql" "toml" "yaml")))
-  value: (block_node
-           (block_scalar) @injection.content))
+; https://yaml-multiline.info#block-scalars
+((block_mapping_pair
+   key: (flow_node (_ (string_scalar) @injection.language))
+   value: (block_node (block_scalar) @injection.content))
+
+ ; skip the block style indicator without considering other indicators
+ (#offset! @injection.content 0 1 0 0)
+
+ ; only consider a few languages for now; case-sensitive
+ (#any-of? @injection.language "json" "sql" "toml" "yaml"))
